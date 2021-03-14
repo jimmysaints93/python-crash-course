81a9648c0b78a1d18d4ce1fbc554302e8e1d31e8
Elizer Dela Cruz <elizer@hubbub.ai>
2021-03-14 13:13:03 +0800

    WIP

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+message = "Hello Python world!"
+print(message)
+
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(f"Hello, {full_name.title()}")
